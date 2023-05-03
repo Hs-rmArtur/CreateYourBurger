9636edd67c9685b6099bc1491678c99d45878315
Mocren <Kl.Artur@web.de>
2023-05-03 21:20:02 +0200

    formatted code

--- src/burgerProgramm/Salat.java
+++ src/burgerProgramm/Salat.java
@@ -1,24 +1,22 @@
 package burgerProgramm;
 
-public class Salat extends Zutat{
-	
+public class Salat extends Zutat {
+
 	public Salat(String name, int nummer, float preis, boolean klassisch, String typ) {
 		super(name, nummer, preis, klassisch, typ);
 	}
-	
+
 	public Salat(String name, int nummer, float preis) {
 		this(name, nummer, preis, false, "vegetarisch");
 	}
-	
-	
-	
+
 	public int zubereiten() {
 		System.out.println(this.name + " wird gewaschen und geschleudert.");
 		return 0;
 	}
-	
+
 	public String toString() {
 		return super.toString();
 	}
-	
+
 }
--- src/burgerProgramm/Zutat.java
+++ src/burgerProgramm/Zutat.java
@@ -7,50 +7,50 @@ public class Zutat {
 	protected boolean vegetarisch;
 	protected boolean vegan;
 	protected boolean klassisch;
-	
+
 	public Zutat(String name, int nummer, float preis, boolean klassisch, String typ) {
 		this.name = name;
 		this.preis = preis;
 		this.klassisch = klassisch;
-		
-		if(typ.equalsIgnoreCase("vegetarisch")) {
+
+		if (typ.equalsIgnoreCase("vegetarisch")) {
 			this.vegetarisch = true;
-		} else if(typ.equalsIgnoreCase("vegan")) {
+		} else if (typ.equalsIgnoreCase("vegan")) {
 			this.vegan = true;
 		}
 	}
-	
+
 	public Zutat(String name, int nummer, float preis, String typ) {
 		this(name, nummer, preis, false, typ);
 	}
-	
+
 	public Zutat(String name, int nummer, float preis) {
 		this(name, nummer, preis, "vegetarisch");
 	}
-	
+
 	public int zubereiten() {
 		System.out.println(this.name + " wird zubereitet.");
 		return 0;
 	}
-	
+
 	public String toString() {
 		String klassisch;
 		String typ = "";
-		
-		if(this.klassisch) {
+
+		if (this.klassisch) {
 			klassisch = "Ja";
 		} else {
 			klassisch = "Nein";
 		}
-		
-		if(this.vegan) {
+
+		if (this.vegan) {
 			typ = "vegan";
-		} else if(this.vegetarisch) {
+		} else if (this.vegetarisch) {
 			typ = "vegetarisch";
 		}
-		
-		return "Name: " + this.name + "  |  Nummer: " + this.nummer + "  |  Preis: " + this.preis +
-				"\nKlassisch: " + klassisch + "   " + typ;
+
+		return "Name: " + this.name + "  |  Nummer: " + this.nummer + "  |  Preis: " + this.preis + "\nKlassisch: "
+				+ klassisch + "   " + typ;
 	}
-	
+
 }
