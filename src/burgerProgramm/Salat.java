238ec3ce9cc263729bcb7fb29e26b0fdeb08775c
Mocren <Kl.Artur@web.de>
2023-05-10 15:27:24 +0200

    added java doc

--- src/burgerProgramm/Salat.java
+++ src/burgerProgramm/Salat.java
@@ -1,20 +1,40 @@
 package burgerProgramm;
-
+/**
+ * Beschreibt ein Salat.
+ * @author Artur Konkel, Michael Karenko
+ *
+ */
 public class Salat extends Zutat {
-
+	
+	/**
+	 * Konstruktor zur Erstellung eines Salats.
+	 * @param name
+	 * @param nummer
+	 * @param preis
+	 * @param klassisch
+	 */
 	public Salat(String name, int nummer, double preis, boolean klassisch) {
 		super(name, nummer, preis, klassisch, VEGAN);
 	}
 	
+	/**
+	 * Berechnung der Hoehe.
+	 */
 	@Override
 	public double berechneHoehe() {
 		return 0;
 	}
-
+	
+	/**
+	 * Ausgabe, wie der Salat zubereitet werden soll.
+	 */
 	public String zubereiten() {
 		return this.name + " wird gewaschen und geschleudert.";
 	}
 	
+	/**
+	 * Berechnung der Zubereitungszeit.
+	 */
 	public int berechneZubereitungsZeit() {
 		return 0;
 	}
